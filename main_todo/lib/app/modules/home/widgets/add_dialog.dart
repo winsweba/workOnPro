92d8544e8992c54ee669c449ef7253a87123f25f
winsweba <winsweb@outlook.com>
2022-01-07 17:06:07 -0800

    wins

--- main_todo/lib/app/modules/home/widgets/add_dialog.dart
+++ main_todo/lib/app/modules/home/widgets/add_dialog.dart
@@ -11,141 +11,144 @@ class AddDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: homeCtrl.formKey,
-        child: ListView(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(3.0.wp),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Get.back();
-                      homeCtrl.editingCtrl.clear();
-                      homeCtrl.changeTask(null);
-                    },
-                    icon: const Icon(Icons.close),
-                  ),
-                  TextButton(
-                      style: ButtonStyle(
-                          overlayColor:
-                              MaterialStateProperty.all(Colors.transparent)),
+    return WillPopScope(
+      onWillPop: ()  async => false,
+      child: Scaffold(
+        body: Form(
+          key: homeCtrl.formKey,
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(3.0.wp),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
                       onPressed: () {
-                      if(homeCtrl.formKey.currentState!.validate()){
-                        if(homeCtrl.task.value == null){
-                          EasyLoading.showError('Please Slecte Task Type');
-                        }
-                        else {
-                          var success = homeCtrl.updateTask(
-                            homeCtrl.task.value!,
-                            homeCtrl.editingCtrl.text,
-                          );
-                          if(success) {
-                            EasyLoading.showSuccess("Todo Add Successfully");
-                            Get.back();
-                            homeCtrl.changeTask(null);
-                          } else {
-                              EasyLoading.showError("TODO item already exist");
-                          }
-                          homeCtrl.editingCtrl.clear();
-                        }
-                      }  
+                        Get.back();
+                        homeCtrl.editingCtrl.clear();
+                        homeCtrl.changeTask(null);
                       },
-                      child: Text(
-                        'Done',
-                        style: TextStyle(fontSize: 14.0.sp),
-                      )),
-                ],
+                      icon: const Icon(Icons.close),
+                    ),
+                    TextButton(
+                        style: ButtonStyle(
+                            overlayColor:
+                                MaterialStateProperty.all(Colors.transparent)),
+                        onPressed: () {
+                        if(homeCtrl.formKey.currentState!.validate()){
+                          if(homeCtrl.task.value == null){
+                            EasyLoading.showError('Please Slecte Task Type');
+                          }
+                          else {
+                            var success = homeCtrl.updateTask(
+                              homeCtrl.task.value!,
+                              homeCtrl.editingCtrl.text,
+                            );
+                            if(success) {
+                              EasyLoading.showSuccess("Todo Add Successfully");
+                              Get.back();
+                              homeCtrl.changeTask(null);
+                            } else {
+                                EasyLoading.showError("TODO item already exist");
+                            }
+                            homeCtrl.editingCtrl.clear();
+                          }
+                        }  
+                        },
+                        child: Text(
+                          'Done',
+                          style: TextStyle(fontSize: 14.0.sp),
+                        )),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
-              child: Text(
-                'New Task',
-                style:
-                    TextStyle(fontSize: 20.0.sp, fontWeight: FontWeight.bold),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+                child: Text(
+                  'New Task',
+                  style:
+                      TextStyle(fontSize: 20.0.sp, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 6.0.wp),
-              child: TextFormField(
-                controller: homeCtrl.editingCtrl,
-                decoration: InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey[400]!),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 6.0.wp),
+                child: TextFormField(
+                  controller: homeCtrl.editingCtrl,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey[400]!),
+                    ),
+                  ),
+                  autofocus: true,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please Enter Your Todo Items';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 5.0.wp,
+                  right: 5.0.wp,
+                  left: 5.0.wp,
+                  bottom: 2.0.wp,
+                ),
+                child: Text(
+                  'Add to',
+                  style: TextStyle(
+                    fontSize: 14.0.sp,
+                    color: Colors.grey,
                   ),
                 ),
-                autofocus: true,
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Please Enter Your Todo Items';
-                  }
-                  return null;
-                },
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 5.0.wp,
-                right: 5.0.wp,
-                left: 5.0.wp,
-                bottom: 2.0.wp,
-              ),
-              child: Text(
-                'Add to',
-                style: TextStyle(
-                  fontSize: 14.0.sp,
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-            ...homeCtrl.tasks
-                .map(
-                  (element) => Obx(() => InkWell(
-                      onTap: () => homeCtrl.changeTask(element),
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 5.0.wp,
-                          vertical: 3.0.wp,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(
-                              IconData(
-                                element.icon,
-                                fontFamily: 'MaterialIcons',
+              ...homeCtrl.tasks
+                  .map(
+                    (element) => Obx(() => InkWell(
+                        onTap: () => homeCtrl.changeTask(element),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 5.0.wp,
+                            vertical: 3.0.wp,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                IconData(
+                                  element.icon,
+                                  fontFamily: 'MaterialIcons',
+                                ),
+                                color: HexColor.fromHex(element.color),
                               ),
-                              color: HexColor.fromHex(element.color),
-                            ),
-                            SizedBox(width: 3.0.wp),
-                            Text(
-                              element.title,
-                              style: TextStyle(
-                                fontSize: 12.0.sp,
-                                fontWeight: FontWeight.bold,
+                              SizedBox(width: 3.0.wp),
+                              Text(
+                                element.title,
+                                style: TextStyle(
+                                  fontSize: 12.0.sp,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                              ],
-                            ),
-                            if(homeCtrl.task.value == element )
-                              const Icon(
-                                Icons.check,
-                                color: Colors.blue
-                              )
-                          ],
+                                ],
+                              ),
+                              if(homeCtrl.task.value == element )
+                                const Icon(
+                                  Icons.check,
+                                  color: Colors.blue
+                                )
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                )
-                .toList()
-          ],
+                  )
+                  .toList()
+            ],
+          ),
         ),
       ),
     );
